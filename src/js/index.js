31f435236cb279dff10c03253e77ed84d42dc8f2
SkyAlumny <72314825+SkyAlumny@users.noreply.github.com>
2021-04-25 20:01:09 +0200

    e

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 
     var $commands = $('#commands-list');
 
-    $.getJSON('https://raw.githubusercontent.com/maisans-maid/Mai/master/assets/json/command-database.json', function(data, status){
+    $.getJSON('https://raw.githubusercontent.com/My-Akeno/command-website/main/commands.json', function(data, status){
 
       $.each(data, function(_, command){
 
